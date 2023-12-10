f302aaa75f054e1d64768705d75beab771eb3b1a
TonyFef <fefilov.toha1999@yandex.ru>
2023-12-10 20:53:28 +0300

    DEV/feat/fixed_logic

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -10,46 +10,62 @@ import { useState } from "react";
 
 export function Question({ questions, setCurrentQuestionIndex }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState("questions");
+  console.log(currentQuestion);
+  const questionActual =
+    currentQuestion === "questions" ? questions : currentQuestion;
 
   function onFormControlLabelClick(e) {
-    const correctAnswer = questions[currentIndex].correctAnswer;
-    console.log("hi", correctAnswer);
-    if (e.target.value === correctAnswer) {
-      alert("Правильно!");
-      setCurrentIndex(currentIndex + 1);
-      setCurrentQuestionIndex(currentIndex + 2);
+    const { target } = e;
+    const { value } = target;
+    console.log(value, questionActual);
+    if (questionActual?.question) {
+      setCurrentQuestion(questionActual[value]);
     } else {
-      alert("Неправильно!");
+      console.log(questionActual);
     }
+    // const correctAnswer = questions[currentIndex].correctAnswer;
+    // if (e.target.value === correctAnswer) {
+    //   alert("Правильно!");
+    //   setCurrentIndex(currentIndex + 1);
+    //   setCurrentQuestionIndex(currentIndex + 2);
+    // } else {
+    //   alert("Неправильно!");
+    // }
   }
 
   return (
     <div className="Quesion">
-      {questions.map(({ question, options }, index) => (
+      <Typography>{questionActual?.question}</Typography>
+      {/* {questionActual.map(({ question, options }, index) => (
         <div
           key={`${question}${Math.random()}`}
           className={index === currentIndex ? "show" : "hide"}
         >
-          <Typography>{question}</Typography>
-          <FormControl>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="none"
-              name="radio-buttons-group"
-            >
-              {options.map(({ value, label }) => (
-                <FormControlLabel
-                  value={value}
-                  control={<Radio />}
-                  label={label}
-                  key={value}
-                  onClick={onFormControlLabelClick}
-                />
-              ))}
-            </RadioGroup>
-          </FormControl>
-        </div>
-      ))}
+          <Typography>{question}</Typography> */}
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="none"
+          name="radio-buttons-group"
+        >
+          {questionActual?.options ? (
+            questionActual?.options.map(({ value, label }) => (
+              <FormControlLabel
+                value={value}
+                control={<Radio />}
+                label={label}
+                key={value}
+                onClick={onFormControlLabelClick}
+              />
+            ))
+          ) : (
+            <Typography>{questionActual?.answer}</Typography>
+          )}
+        </RadioGroup>
+      </FormControl>
+      {/* //   </div> */}
+      {/* // ))} */}
     </div>
   );
 }
