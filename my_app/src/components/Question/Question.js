69d6994aa75f5d34166df66e7f0498b2185062f0
TonyFef <fefilov.toha1999@yandex.ru>
2023-12-10 11:06:36 +0300

    DEV/feat/added_handlers

--- my_app/src/components/Question/Question.js
+++ my_app/src/components/Question/Question.js
@@ -1,19 +1,35 @@
+import "./Question.css";
 import {
   Typography,
   FormControl,
-  FormLabel,
   FormControlLabel,
   Radio,
   RadioGroup,
 } from "@mui/material";
-import { Fragment } from "react";
+import { useState } from "react";
 
-export function Question({questions}) {
-    console.log(questions);
+export function Question({ questions, setCurrentQuestionIndex }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function onFormControlLabelClick(e) {
+    const correctAnswer = questions[currentIndex].correctAnswer;
+    console.log("hi", correctAnswer);
+    if (e.target.value === correctAnswer) {
+      alert("Правильно!");
+      setCurrentIndex(currentIndex + 1);
+      setCurrentQuestionIndex(currentIndex + 2);
+    } else {
+      alert("Неправильно!");
+    }
+  }
+
   return (
     <div className="Quesion">
-      {questions.map(({ question, options }) => (
-        <Fragment key={`${question}${Math.random()}`}>
+      {questions.map(({ question, options }, index) => (
+        <div
+          key={`${question}${Math.random()}`}
+          className={index === currentIndex ? "show" : "hide"}
+        >
           <Typography>{question}</Typography>
           <FormControl>
             <RadioGroup
@@ -27,11 +43,12 @@ export function Question({questions}) {
                   control={<Radio />}
                   label={label}
                   key={value}
+                  onClick={onFormControlLabelClick}
                 />
               ))}
             </RadioGroup>
           </FormControl>
-        </Fragment>
+        </div>
       ))}
     </div>
   );
